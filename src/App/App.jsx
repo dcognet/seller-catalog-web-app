493b26bd5e33f82a5eb203b1464e41b3daed8bf9
damien.cognet <damien.cognet@cdbiz.com>
2023-07-21 13:47:41 +0200

    feat: 🎸 add MUI theme, add AppBar to Template

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,12 +1,19 @@
 import { RouterProvider } from "react-router-dom";
-// import Header from "../Header";
 import "./app.css";
 import { router } from "./router.jsx";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
 
 export default function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </>
   );
 }
--- src/Template/index.jsx
+++ src/Template/index.jsx
@@ -1,9 +1,107 @@
-import Header from "@/Header";
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import AdbIcon from "@mui/icons-material/Adb";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Link } from "react-router-dom";
+
+const mainMenu = [
+  {
+    name: "Produit",
+    url: "/",
+  },
+  {
+    name: "Commandes",
+    url: "/orders",
+  },
+  {
+    name: "Finance",
+    url: "/financial",
+  },
+  {
+    name: "Rapports",
+    url: "/reports",
+  },
+  {
+    name: "Messages",
+    url: "/messages",
+  },
+  {
+    name: "Marketplaces",
+    url: "/marketplaces",
+  },
+];
 
 export default function Template() {
   return (
     <>
-      <Header />
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component={Link}
+              to="/"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              Seller Catalog
+            </Typography>
+
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+            </Box>
+            <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              {mainMenu.map(({ name, url }) => (
+                <Button
+                  key={url}
+                  component={Link}
+                  to={url}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  {name}
+                </Button>
+              ))}
+            </Box>
+
+            <Tooltip title="Open settings">
+              <IconButton sx={{ p: 0 }}>
+                <Avatar
+                  alt="Remy Sharp"
+                  src="christopher-campbell-rDEOVtE7vOs-unsplash.jpg"
+                />
+              </IconButton>
+            </Tooltip>
+          </Toolbar>
+        </Container>
+      </AppBar>
     </>
   );
 }
