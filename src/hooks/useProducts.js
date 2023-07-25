6f3e7f4aa5ef4e221eff0fb921b7eb93f1d965fd
damien.cognet <damien.cognet@cdbiz.com>
2023-07-25 17:28:41 +0200

    feat: 🎸 add filter

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,10 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import api from "./../api";
 
-export default function useProducts() {
-  const { isLoading, data: products } = useQuery(
+export default function useProducts({ search }) {
+  const {
+    isLoading,
+    data: products,
+    refetch,
+  } = useQuery(
     ["repoData"],
-    async () => await api.get("products").json()
+    async () => await api.get(`products?q=${search}`).json()
   );
-  return { isLoading, products };
+  return { isLoading, products, refetch };
 }
--- src/products/ProductsRoute/ProductsRoute.jsx
+++ src/products/ProductsRoute/ProductsRoute.jsx
@@ -14,6 +14,8 @@ import { useProducts } from "../../hooks";
 import ProductDescription from "../ProductDescription";
 import Page from "./../../ds/Pages";
 import { Search } from "@mui/icons-material";
+import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 const columns = [
   {
@@ -37,7 +39,10 @@ const columns = [
 ];
 
 export default function ProductsRoute() {
-  const { isLoading, products } = useProducts();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const [search, setSearch] = useState(searchParams.get("q") || "");
+  const { isLoading, products, refetch } = useProducts({ search });
 
   if (isLoading) return <LinearProgress />;
 
@@ -46,6 +51,8 @@ export default function ProductsRoute() {
       <Page title="Gestion du catalog">
         <Box sx={{ ml: 21, py: 8 }}>
           <TextField
+            value={search}
+            onChange={(event) => setSearch(event.currentTarget.value)}
             id="input-with-icon-textfield"
             placeholder="Recherche par GTIN/SKU"
             InputProps={{
@@ -56,8 +63,24 @@ export default function ProductsRoute() {
               ),
             }}
             sx={{ width: 630 }}
+            onKeyDown={(event) => {
+              console.log(event.key);
+              if (event.key == "Enter") {
+                setSearchParams({ q: search });
+                refetch();
+              }
+            }}
+            autoComplete="off"
           />
-          <Button sx={{ ml: 1 }}>Rechercher</Button>
+          <Button
+            sx={{ ml: 1 }}
+            onClick={() => {
+              setSearchParams({ q: search });
+              refetch();
+            }}
+          >
+            Rechercher
+          </Button>
         </Box>
         <Box sx={{ height: "100%", width: "100%" }}>
           <DataGrid
