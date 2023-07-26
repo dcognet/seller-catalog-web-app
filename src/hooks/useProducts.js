a8bb4813e14f8a016aa1adeba255c48c5b224a76
damien.cognet <damien.cognet@cdbiz.com>
2023-07-26 12:37:59 +0200

    feat: 🎸 improve product page

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,14 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import api from "./../api";
 
-export default function useProducts({ search }) {
+export default function useProducts({ search, id }) {
   const {
     isLoading,
     data: products,
     refetch,
-  } = useQuery(
-    ["repoData"],
-    async () => await api.get(`products?q=${search}`).json()
-  );
+  } = useQuery({
+    queryKey: id ? ["products", id] : ["products"],
+    queryFn: async () => {
+      const apiRoute = id ? `products/${id}` : `products?q=${search}`;
+      return await api.get(apiRoute).json();
+    },
+  });
   return { isLoading, products, refetch };
 }
--- src/products/ProductRoute/ProductRoute.jsx
+++ src/products/ProductRoute/ProductRoute.jsx
@@ -1,7 +1,87 @@
 import { useParams } from "react-router-dom";
+import { useProducts } from "../../hooks";
+import {
+  Alert,
+  AlertTitle,
+  Button,
+  LinearProgress,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import Page from "../../ds/Pages/";
+import { Edit, Favorite } from "@mui/icons-material";
+import { Box } from "@mui/system";
+import { Price } from "../../ds/atoms";
+import ProductCondition from "../ProductCondition/";
+
+const defaultRenderCell = ({ value }) => value;
 
 export default function ProductRoute() {
   let { id } = useParams();
+  const { isLoading, products: product } = useProducts({ id });
 
-  return `Product id = ${id}`;
+  if (isLoading) return <LinearProgress />;
+
+  const productProperties = [
+    { name: "Nom", value: product.name },
+    { name: "Prix", value: product.price, type: "number", renderCell: Price },
+    {
+      name: "Condition",
+      value: product.condition == "used",
+      renderCell: ProductCondition,
+    },
+    { name: "Stock", value: product.stock, type: "number" },
+  ];
+
+  return (
+    <Page title={`Product: ${product.name}`}>
+      <Alert severity="success">
+        <AlertTitle>Votre fiche produit a été enrichie avec succées</AlertTitle>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur
+        voluptatibus vel inventore earum, dignissimos voluptate consectetur
+        officia saepe impedit, cupiditate doloribus aliquam dolorem minus error.
+        Beatae quia excepturi tempora accusantium.
+      </Alert>
+      <Box sx={{ display: "flex", justifyContent: "flex-end", gap: 1, my: 2 }}>
+        <Button variant="outlined" startIcon={<Favorite />}>
+          Ajouter aux favoris
+        </Button>
+
+        <Button startIcon={<Edit />}>Editer</Button>
+      </Box>
+
+      <Box component="artcile" sx={{ display: "flex", gap: 2 }}>
+        <img src={product.imageUrl} alt={product.name} width="300" />
+        <TableContainer>
+          <Typography variant="h6">Propriétés</Typography>
+          <Table sx={{ minWidth: 650 }} aria-label="Fiche produit">
+            <TableBody>
+              {productProperties.map(
+                ({ name, value, type, renderCell = defaultRenderCell }) => (
+                  <TableRow
+                    key={name}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {name}
+                    </TableCell>
+                    <TableCell
+                      align={type === "number" ? "right" : "left"}
+                      sx={{ fontWeight: "bold" }}
+                    >
+                      {renderCell({ value })}
+                    </TableCell>
+                  </TableRow>
+                )
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Page>
+  );
 }
